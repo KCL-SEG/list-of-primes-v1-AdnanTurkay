b63e0d6b51b4d8c4536dad8f4eeb6fc12d4e0032
AdnanTurkay <adntrky@gmail.com>
2022-11-25 02:51:39 +0000

    Initial solution for "List of primes v1"

--- primes.py
+++ primes.py
@@ -1,6 +1,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+
+    count = 2
+    while number_of_primes != 0:
+        prime = True
+
+        for i in range(2, count):
+            if count % i == 0:
+                prime = False
+
+        if prime:
+            list.append(count)
+            number_of_primes -= 1
+
+        count += 1
+
     return list
